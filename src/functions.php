e0b5674c3a18b22386bf96c86bba0e01d170ed3c
marope01 <marope01@alumnatflorida.es>
2025-11-04 09:59:25 +0100

    sisisisissisi

--- src/functions.php
+++ src/functions.php
@@ -1,10 +1,7 @@
 <?php function saludar() { return "Hola desde la función!"; } ?>
 <?php function saludarPersonalizado($nombre) { return "Hola, $nombre!"; 
 } ?>
-<<<<<<< HEAD
 <?php function saludarPersonalizado($nombre) { return "Hola, $nombre!"; 
 } ?>
-=======
 <?php function saludarPersonalizado($nombre) { return "¡Hola, 
 $nombre!"; } ?>
->>>>>>> funcionalidad-1
